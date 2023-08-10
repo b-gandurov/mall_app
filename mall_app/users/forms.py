8b68ab7c7e5ce6b2153c4de4663b1c09359f2ba1
Божил Гандуров <bozhil.gandurov@gmail.com>
2023-08-10 20:15:25 +0300

    added pasworord change option, reformated code

--- mall_app/users/forms.py
+++ mall_app/users/forms.py
@@ -1,10 +1,60 @@
 from django import forms
-
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import PasswordResetForm
 from mall_app.users.models import UserProfile
+from django.contrib.auth import forms as auth_forms
+
+UserModel = get_user_model()
 
 
 class UserProfileForm(forms.ModelForm):
+    new_password = forms.CharField(widget=forms.PasswordInput(), required=False, label="New Password")
+    confirm_password = forms.CharField(widget=forms.PasswordInput(), required=False, label="Confirm New Password")
+
     class Meta:
         model = UserProfile
         exclude = ('user',)
 
+    def clean(self):
+        cleaned_data = super().clean()
+        new_password = cleaned_data.get("new_password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if new_password or confirm_password:
+            if new_password != confirm_password:
+                raise ValidationError("The new passwords do not match")
+
+            # Use Django's built-in password validation
+            validate_password(new_password, self.instance.user)
+
+        return cleaned_data
+
+    def save(self, commit=True):
+        user = self.instance.user
+        new_password = self.cleaned_data.get("new_password")
+
+        if new_password:
+            user.set_password(new_password)
+            user.save()
+
+        return super().save(commit=commit)
+
+
+class CustomPasswordResetForm(PasswordResetForm):
+    def get_users(self, email):
+        """Return matching user objects for the given email."""
+        email_field_name = get_user_model().EMAIL_FIELD
+        for user in get_user_model()._default_manager.filter(**{
+            '%s__iexact' % email_field_name: email}):
+            if user.has_usable_password() and user.email_user:
+                yield user
+        else:
+            self.add_error(None, "Email is not registered")
+
+
+class RegisterUserForm(auth_forms.UserCreationForm):
+    class Meta(auth_forms.UserCreationForm.Meta):
+        model = UserModel
+        fields = ('email',)
--- mall_app/users/views.py
+++ mall_app/users/views.py
@@ -1,37 +1,29 @@
-from datetime import datetime, timedelta
-
-from django.contrib.auth import views as auth_views, login, get_user_model
-from django.contrib.auth import forms as auth_forms
+from datetime import timedelta
+from django.contrib import messages
+from django.contrib.auth import views as auth_views, login, get_user_model, update_session_auth_hash
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
-from django.http import HttpResponseNotAllowed, JsonResponse
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.utils import timezone
 from django.utils.decorators import method_decorator
 from django.views import generic as views
-
 from django.shortcuts import redirect
 from django.views.generic import DeleteView
-
 from mall_app.cinema.models import Ticket
 from mall_app.stores.models import Reservation
-from mall_app.users.forms import UserProfileForm
+from mall_app.users.forms import UserProfileForm, RegisterUserForm
 from mall_app.users.models import UserProfile
-from django.contrib.auth.forms import PasswordResetForm
+
 
 def sort_reservations_by_remaining_time(reservations):
-    # Get the current time
     now = timezone.now()
-
-    # Add a computed property to each reservation representing the remaining time
     for reservation in reservations:
         reservation.remaining_time = (reservation.reservation_time + timedelta(minutes=5)) - now
-
-    # Sort the reservations by the remaining time
     sorted_reservations = sorted(reservations, key=lambda r: (r.remaining_time < timedelta(0), r.remaining_time))
-
     return sorted_reservations
+
 
 def anonymous_required(view_func):
     def _wrapped_view_func(request, *args, **kwargs):
@@ -47,12 +39,6 @@ UserModel = get_user_model()
 
 def index(request):
     return render(request, 'index.html')
-
-
-class RegisterUserForm(auth_forms.UserCreationForm):
-    class Meta(auth_forms.UserCreationForm.Meta):
-        model = UserModel
-        fields = ('email',)
 
 
 class RegisterUserView(views.CreateView):
@@ -86,44 +72,34 @@ class LoginUserView(auth_views.LoginView):
         return context
 
 
-
-
-
-# class UserProfileView(views.UpdateView):
-#     model = UserProfile
-#     form_class = UserProfileForm
-#     template_name = 'user_profile.html'
-#     success_url = reverse_lazy('index')
-#
-#     def get_object(self, queryset=None):
-#         return self.request.user
-#
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         context['reservations'] = self.request.user.reservations.all()
-#         return context
-
-class UserProfileView(views.UpdateView):
+class UserProfileView(LoginRequiredMixin, views.UpdateView):
     model = UserProfile
     form_class = UserProfileForm
     template_name = 'user_profile.html'
-    success_url = reverse_lazy('index')
+    success_url = reverse_lazy('profile')
 
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     reservations = Reservation.objects.filter(user=self.request.user)
-    #     sorted_reservations = sort_reservations_by_remaining_time(reservations)
-    #     context['reservations'] = sorted_reservations
-    #     # context['reservations'] = Reservation.objects.filter(user=self.request.user)
-    #     return context
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        new_password = form.cleaned_data.get('new_password')
+
+        if new_password:
+            user = self.request.user
+            user.set_password(new_password)
+            user.save()
+            user.refresh_from_db()
+            update_session_auth_hash(self.request, user)
+            messages.success(self.request, 'Your password was changed successfully.')  # This line
+
+        return response
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         claimed_reservations = Reservation.objects.filter(user=self.request.user, is_claimed=True)
         unclaimed_reservations = Reservation.objects.filter(user=self.request.user, is_claimed=False)
         sorted_unclaimed_reservations = sort_reservations_by_remaining_time(unclaimed_reservations)
-        # Create a Paginator object
-        paginator = Paginator(sorted_unclaimed_reservations, 3)  # 3 items per page
-        page_unclaimed = self.request.GET.get('page_unclaimed', 1)  # Get the page number from the request
+
+        paginator = Paginator(sorted_unclaimed_reservations, 3)
+        page_unclaimed = self.request.GET.get('page_unclaimed', 1)
         unclaimed_reservations_page = paginator.get_page(page_unclaimed)
 
         paginator_claimed = Paginator(claimed_reservations, 3)
@@ -138,10 +114,8 @@ class UserProfileView(views.UpdateView):
         return context
 
     def get_initial(self):
-        # Get the initial dictionary from the superclass method
-        initial = super(UserProfileView, self).get_initial()
 
-        # Update the dictionary with the UserProfile field values
+        initial = super(UserProfileView, self).get_initial()
         user_profile = self.get_object()
         initial['first_name'] = user_profile.first_name
         initial['last_name'] = user_profile.last_name
@@ -170,28 +144,16 @@ class UserProfileDeleteView(LoginRequiredMixin, DeleteView):
         return self.request.user
 
 
-class CustomPasswordResetForm(PasswordResetForm):
-    def get_users(self, email):
-        """Return matching user objects for the given email."""
-        email_field_name = get_user_model().EMAIL_FIELD
-        for user in get_user_model()._default_manager.filter(**{
-                '%s__iexact' % email_field_name: email}):
-            if user.has_usable_password() and user.email_user:
-                yield user
-        else: # Add this line
-            self.add_error(None, "Email is not registered") # And this line
-
-
 def increase_item_quantity(request):
     reservation_id = request.GET.get('reservation_id')
     reservation = Reservation.objects.get(id=reservation_id)
 
-    # Check whether the item's quantity has already been increased
+
     if not reservation.item_quantity_increased:
         item = reservation.item
         item.quantity += 1
         item.save()
-        # Set the flag to True to indicate that the item's quantity has been increased
+
         reservation.item_quantity_increased = True
         reservation.save()
         return JsonResponse({'status': 'success'})
